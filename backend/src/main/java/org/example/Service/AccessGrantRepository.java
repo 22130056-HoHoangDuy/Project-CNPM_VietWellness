ff6123f2cbe0bae5dbf531a66e5fe552dabb289b
Bao <thaibaothptdl@gmail.com>
2025-05-25 19:19:15 +0700

    commit Refactoring

--- backend/src/main/java/org/example/Service/AccessGrantRepository.java
+++ backend/src/main/java/org/example/Service/AccessGrantRepository.java
@@ -5,7 +5,10 @@ import org.example.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccessGrantRepository extends JpaRepository<AccessGrant, Long> {
     List<AccessGrant> findByUser(User user);
+
+    Optional<AccessGrant> findByUserAndGranteeName(User user, String granteeName);
 }
